9a110472b7e8e44b4eec00cb450d79b0206d1d09
mohapatrasamir <mohapatra.samir1998@gmail.com>
2022-04-29 12:19:27 +0530

    iteration using for each loop

--- src/com/java/collections/AlBasics.java
+++ src/com/java/collections/AlBasics.java
@@ -19,6 +19,8 @@ public class AlBasics {
         al.add("Samuel");
         al.add("Arman");
         al.add("Pomila");
+        al.add("Ritvik");
+        al.add("Daneil");
 
         //printing elements
         System.out.println(al);
@@ -38,13 +40,24 @@ public class AlBasics {
         al.remove(2) ; //passing index
         System.out.println(al);
 
+        al.remove("Ritvik"); //passing value
+        System.out.println(al);
+
         //size of AL
         System.out.println(al.size()); //returns size of int type
 
-        //iteration
+        //iteration - for loop
+        System.out.println("Iteration using for loop");
         for(int i=0; i<al.size(); i++) {
             System.out.println(al.get(i));
         }
+
+        //iteration - for each loop
+        System.out.println("Iteration using for each loop");
+        for(String i : al){
+            System.out.print(i + " ") ;
+        }
+        System.out.println() ;
 
         //sorting
         Collections.sort(al) ; //by default ascending
