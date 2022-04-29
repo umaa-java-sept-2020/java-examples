11c2050cc3daaaaf33280d279ba79399d7d8870b
mohapatrasamir <mohapatra.samir1998@gmail.com>
2022-04-29 12:12:57 +0530

    removing elements by passing value

--- src/com/java/collections/AlBasics.java
+++ src/com/java/collections/AlBasics.java
@@ -1,2 +1,58 @@
-package com.java.collections;public class AlBasics {
+// DISCLAIMER :- this code contain basics and all operations on ArrayList
+
+//AL can't store primitive data types (int,char, double,long)
+//Integer, String, Long, Float, Double, Boolean, Character, Byte, Short
+
+import java.util.ArrayList ;
+import java.util.Collections ;
+import java.util.List ;
+
+public class AlBasics {
+
+    public static void main(String args[]){
+
+        ArrayList <String> al = new ArrayList<> () ;
+        // using list to declare arraylist syntax check and confirm
+        // List <Integer> li = new List<>();
+
+        //adding elements
+        al.add("Samuel");
+        al.add("Arman");
+        al.add("Pomila");
+
+        //printing elements
+        System.out.println(al);
+
+        //get specific elements
+        System.out.println(al.get(1)); //passing index in the parameter
+
+        //adding element in between the indexes
+        al.add(1,"Ramez");
+        System.out.println(al);
+
+        //set element - replacing element
+        al.set(1, "Rahul");
+        System.out.println(al);
+
+        //delete element
+        al.remove(2) ; //passing index
+        System.out.println(al);
+
+        //size of AL
+        System.out.println(al.size()); //returns size of int type
+
+        //iteration
+        for(int i=0; i<al.size(); i++) {
+            System.out.println(al.get(i));
+        }
+
+        //sorting
+        Collections.sort(al) ; //by default ascending
+        System.out.println(al);
+
+        Collections.sort(al, Collections.reverseOrder()); // descending order
+        System.out.println(al);
+
+    }
+
 }
--- src/com/java/collections/ArrayListMain.java
+++ src/com/java/collections/ArrayListMain.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class ArrayListMain {
 
-    private static void display(List<Integer> list){
-        for(Integer i: list ){
+    private static void display(List<Integer> list) {
+        for (Integer i : list) {
             System.out.println(i);
         }
 
@@ -17,7 +17,8 @@ public class ArrayListMain {
 
         m1();
     }
-    public static void m1(){
+
+    public static void m1() {
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(45);
