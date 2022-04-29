b218830965d5005e4d7826c4336ea642c13ca5b1
mohapatrasamir <mohapatra.samir1998@gmail.com>
2022-04-29 10:48:02 +0530

    ArrayList add and display

--- src/com/java/collections/ArrayListMain.java
+++ src/com/java/collections/ArrayListMain.java
@@ -1,2 +1,29 @@
-package com.java.collections;public class ArrayListMain {
+package com.java.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListMain {
+
+    private static void display(List<Integer> list){
+        for(Integer i: list ){
+            System.out.println(i);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        m1();
+    }
+    public static void m1(){
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(45);
+        list.add(32);
+
+        display(list);
+
+    }
 }
