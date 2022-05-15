0a77ad1b0b5c1faccf25d146233d80d12cd83d51
mohapatrasamir <mohapatra.samir1998@gmail.com>
2022-05-15 09:52:38 +0530

    vector complete

--- src/com/java/collections/vector/VBasics.java
+++ src/com/java/collections/vector/VBasics.java
@@ -2,10 +2,8 @@ package com.java.collections.vector;
 
 import com.sun.xml.internal.ws.addressing.WsaActionUtil;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.Vector;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class VBasics {
 
@@ -50,6 +48,25 @@ public class VBasics {
         while(e.hasMoreElements())
             System.out.println(e.nextElement());
 
+//        iteration through Iterator
+        System.out.println("Iteration through Iterator ");
+        Iterator itr = v.iterator();
+        while(itr.hasNext())
+            System.out.println(itr.next());
+
+//        iteration through ListIterator
+        System.out.println("Iteartion through ListIterator \n forward ");
+        ListIterator litr = v.listIterator();
+        while(litr.hasNext())
+            System.out.println(litr.next());
+
+        System.out.println("Iteartion through ListIterator \n backward ");
+        while(litr.hasPrevious())
+            System.out.println(litr.previous());
+//        HOW ?
+//        we can perform several other operation using methods of ListIterator API
+//        like displaying the indexes of next and previous elements, replacing the element value, remove elements during iteration etc.
+
 //        setSize() -> this will reduce or increase the size and remove elements if size reduced but more element there
 //        v.setSize(5) ;
 //        System.out.println("size after setSize(5) = " + v.size());
@@ -86,9 +103,17 @@ public class VBasics {
 //        System.out.println("removing 444 " + v.removeElement(444));
 //        System.out.println(v);
 
+//        remove(index)
+        System.out.println("removing index 2 " + v.remove(1) );
+        System.out.println("v = " + v);
+
 //        removeAll(Collection c) -> remove every occurrence
         System.out.println("removeAll() = " + v.removeAll(al));
         System.out.println(v);
+
+//        clear() -> remove all elements
+//        v.clear();
+//        System.out.println("clear = " + v);
 
 ////       setElementAt
         v.setElementAt(333, 3);
@@ -96,6 +121,10 @@ public class VBasics {
         v.setElementAt(324, 2);
         v.setElementAt(77, 4);
         System.out.println(v);
+
+//        set(index, element)
+        v.set(2, 423);
+        System.out.println("set index 2 = " + v);
 
 //        subList
         System.out.println("subList = " + v.subList(1,5));
@@ -129,8 +158,27 @@ public class VBasics {
         v1.add(444);
         System.out.println("v before copy = " + v);
         System.out.println("v1 = " + v1);
-        Collections.copy(v, v1); //it copies elements and replaces the existing elements of second vector
+        Collections.copy(v, v1);
+//        it copies elements and replaces the existing elements of second vector
+//        the second vector must be larger or equal to the first vector which is being copied
         System.out.println("v after copy = " + v);
+
+
+//        Vector -> List
+        List <Integer> li = Collections.list(v.elements());
+        System.out.println("list = " + li);
+
+//        Vector -> ArrayList
+        ArrayList <Integer> al1 = new ArrayList<>(v) ;
+        System.out.println("ArrayList1 = " + al1);
+
+//        Vector -> Array
+        System.out.println("vector to array");
+        Integer arr2[] = (Integer[]) v.toArray(new Integer [v.size()]);
+        for(int i : arr2)
+            System.out.println(i);
+
+
 
 
 
